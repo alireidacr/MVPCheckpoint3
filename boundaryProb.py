d0be9b3a6d126bd4de559aca85e9ec0008b257d6
alireidacr <46281689+alireidacr@users.noreply.github.com>
2019-04-03 18:09:39 +0100

    Add files via upload

--- boundaryProb.py
+++ boundaryProb.py
@@ -3,11 +3,14 @@
 import sys
 import random as rnd
 import numpy as np
+import math
 
 def getInputParams():
     dimensions = int(sys.argv[1])
+    accuracy = float(sys.argv[2])
+    type = sys.argv[3]
 
-    return dimensions
+    return dimensions, accuracy, type
 
 def generateInitState(dimensions):
     lattice = np.zeros((dimensions, dimensions, dimensions))
@@ -19,7 +22,7 @@ def generateInitState(dimensions):
     return lattice
 
 
-def jacobiUpdate(pos, potentialLat, charge):
+def jacobiHelper(pos, potentialLat, charge):
     intermediate = (potentialLat[(pos[0]+1, pos[1], pos[2])] + potentialLat[(pos[0]-1, pos[1], pos[2])]
                   + potentialLat[(pos[0], pos[1]+1, pos[2])] + potentialLat[(pos[0], pos[1]-1, pos[2])]
                   + potentialLat[(pos[0], pos[1], pos[2]+1)] + potentialLat[(pos[0], pos[1], pos[2]-1)])
@@ -27,16 +30,28 @@ def jacobiUpdate(pos, potentialLat, charge):
     return (1/6)*(intermediate + charge)
 
 
-def updateLattice(chargeLat, potentialLat, dimensions):
+def jacobiUpdate(chargeLat, potentialLat, dimensions):
     returnLat = np.zeros((dimensions, dimensions, dimensions))
 
     for pos, state in np.ndenumerate(potentialLat):
         if (dimensions -1 in pos or 0 in pos):
             returnLat[pos] = 0
         else:
-            returnLat[pos] = jacobiUpdate(pos, potentialLat, chargeLat[pos])
+            returnLat[pos] = jacobiHelper(pos, potentialLat, chargeLat[pos])
 
-    potentialLat = returnLat
+    return returnLat
+
+
+def GSUpdate(chargeLat, potentialLat, dimensions):
+    returnLat = np.copy(potentialLat)
+
+    for pos, state in np.ndenumerate(returnLat):
+        if (dimensions -1 in pos or 0 in pos):
+            returnLat[pos] = 0
+        else:
+            returnLat[pos] = jacobiHelper(pos, potentialLat, chargeLat[pos])
+
+    return returnLat
 
 def writeElectricField(potentialLat, dimensions):
     eFile = open("electricField.txt", "a+")
@@ -54,18 +69,94 @@ def writeElectricField(potentialLat, dimensions):
 
     eFile.close()
 
+def writePotential(potentialLat, centre):
+    potFile = open("Potential.txt", "a+")
+    potSlice = open("PotentialSlice.txt", "a+")
+
+    for pos, state in np.ndenumerate(potentialLat):
+        potFile.write(str(pos[0]) + " " + str(pos[1]) + " " + str(pos[2]) + " " + str(state) + "\n")
+
+        if (pos[2] == centre):
+            potSlice.write(str(pos[0]) + " " + str(pos[1]) + " " + str(state) + "\n")
+
+    potFile.close()
+    potSlice.close()
+
+def writeEFieldSlice(potentialLat, dimensions, centre):
+    eSliceFile = open("ESlice.txt", "a+")
+    eMagFile = open("EMag.txt", "a+")
+
+    for pos, state in np.ndenumerate(potentialLat):
+        if (pos[1] == centre and not 0 in pos and not dimensions-1 in pos):
+            xComp = (potentialLat[(pos[0]+1, pos[1], pos[2])] - potentialLat[(pos[0]-1, pos[1], pos[2])])/2
+            zComp = (potentialLat[pos[0], pos[1], pos[2]+1] - potentialLat[pos[0], pos[1], pos[2]-1])/2
+            eSliceFile.write(str(pos[0]) + " " + str(pos[2]) + " " + str(-xComp) + " " + str(-zComp) + "\n")
+
+            rad = math.sqrt((pos[0] - centre)**2 + (pos[2] - centre)**2)
+            eMag = math.sqrt(xComp**2 + zComp**2)
+            eMagFile.write(str(rad) + " " + str(eMag) + "\n")
+
+    eSliceFile.close()
+    eMagFile.close()
+
+
+def findMaxDif(oldLat, newLat):
+    maxDiff = 0
+    for pos, state in np.ndenumerate(oldLat):
+        if (abs(state - newLat[pos]) > maxDiff):
+            maxDiff = abs(state - newLat[pos])
+
+    return maxDiff
+
+def writeMagField(potentialLat, dimensions, centre):
+    magFile = open("magField.txt", "a+")
+    magSliceFile = open("magFieldSlice.txt", "a+")
+
+    for pos, state in np.ndenumerate(potentialLat):
+        if (not 0 in pos and not dimensions-1 in pos):
+            xComp = (potentialLat[(pos[0], pos[1]+1, pos[2])] - potentialLat[(pos[0], pos[1]-1, pos[2])])
+            yComp = (potentialLat[(pos[0]+1, pos[1], pos[2])] - potentialLat[(pos[0]-1, pos[1], pos[2])])
+            zComp = 0
+
+            magFile.write(str(pos[0]) + " " + str(pos[1]) + " " + str(pos[2]) + " " + str(xComp) + " " + str(yComp) + " " + str(zComp) + "\n")
+            if (pos[2] == centre):
+                magSliceFile.write(str(pos[0]) + " " + str(pos[1]) + " " + str(xComp) + " " + str(yComp) + "\n")
+
+    magFile.close()
+    magSliceFile.close()
+
+
 def main():
-    dimensions = getInputParams()
+    dimensions, accuracy, type = getInputParams()
+
+    if (type.lower() == "jacobi"):
+        updateLattice = jacobiUpdate
+    else:
+        updateLattice = GSUpdate
 
     chargeLat = np.zeros((dimensions, dimensions, dimensions))
     centre = int(dimensions/2)
-    chargeLat[(centre, centre, centre)] = 1 # place a point charge at the centre of the box
+    #chargeLat[(centre, centre, centre)] = 10 # place a point charge at the centre of the box
+    for i in range(dimensions):
+        pos = (centre, centre, i)
+        chargeLat[pos] = 10
+
     potentialLat = generateInitState(dimensions)
 
-    for sweep in range(10000):
-        updateLattice(chargeLat, potentialLat, dimensions)
-        print(sweep)
+    maxDiff = accuracy + 1
+    sweep = 0
+    while maxDiff > accuracy:
 
-    writeElectricField(potentialLat, dimensions)
+        returnLat = updateLattice(chargeLat, potentialLat, dimensions)
+        maxDiff = findMaxDif(potentialLat, returnLat)
+        potentialLat = returnLat
+        sweep += 1
+        print(sweep)
+        print("max Diff: " + str(maxDiff))
+
+    #writeElectricField(potentialLat, dimensions)
+    #writeEFieldSlice(potentialLat, dimensions, centre)
+    writePotential(potentialLat, centre)
+    writeMagField(potentialLat, dimensions, centre)
 
 main()
--- cahnHilliard.py
+++ cahnHilliard.py
@@ -15,24 +15,31 @@ def getInputParams():
     tempDisc = float(sys.argv[6])
     initVal = float(sys.argv[7])
 
+    print("dimensions: " + str(dimensions))
+    print("mobility: " + str(mobility))
+    print("kappa: " + str(kappa))
+    print("aConst: " + str(aConst))
+    print("spatDisc:" + str(spatDisc))
+    print("tempDisc: " + str(tempDisc))
+    print("initVal: " + str(initVal))
     return dimensions, mobility, kappa, aConst, spatDisc, tempDisc, initVal
 
 def generateInitState(dimensions, initVal):
     grid = np.zeros((dimensions, dimensions))
     for pos, state in np.ndenumerate(grid):
-        grid[pos] = initVal + rnd.uniform(-0.1, 0.1)
+        grid[pos] = initVal + (rnd.random() - 0.5) * 2 * 0.01
 
     return grid
 
 def findChemPotential(lattice, kappa, aConst, pos, spatDisc):
     NNs = lattice.getNearestNeighbours(pos)
 
-    chemPotential = -(aConst * lattice[pos]) + (aConst * lattice[pos]**3)
-    kappaCont = sum([lattice[NN] for NN in NNs]) - (4 * lattice[pos])
+    intermediate1 = aConst * (lattice[pos]**3 - lattice[pos])
+    intermediate2 = sum([lattice[NN] for NN in NNs]) - (4.0 * lattice[pos])
+    intermediate3 = ((kappa / spatDisc**2) * intermediate2)
 
-    chemPotential -= (kappa / spatDisc**2) * kappaCont
+    chemPotential = intermediate1 + intermediate3
     return chemPotential
-
 
 
 def updateLattice(lattice, mobility, kappa, aConst, spatDisc, tempDisc):
@@ -40,20 +47,36 @@ def updateLattice(lattice, mobility, kappa, aConst, spatDisc, tempDisc):
 
     for pos, state in np.ndenumerate(lattice.getLattice()):
         chemPot = findChemPotential(lattice, kappa, aConst, pos, spatDisc)
-
-        state = lattice[pos]
         NNs = lattice.getNearestNeighbours(pos)
-        mobCont =  sum([findChemPotential(lattice, kappa, aConst, NN, spatDisc) for NN in NNs]) - (4 * chemPot)
-        returnGrid[pos] = state + ((mobility * tempDisc)/(spatDisc**2) * mobCont)
 
-    lattice.setLattice(returnGrid);
+        mobCont = sum([findChemPotential(lattice, kappa, aConst, NN, spatDisc) for NN in NNs]) - (4 * chemPot)
+        factor = (mobility * tempDisc)/(spatDisc**2)
+        returnGrid[pos] = state + (factor * mobCont)
+
+    lattice.setLattice(returnGrid)
+
+
+def freeEnergy(lattice, aConst, kappa, spatDisc):
+    intermediate1 = 0
+    intermediate2 = 0
+    intermediate3 = 0
+
+    for pos, state in np.ndenumerate(lattice.getLattice()):
+         intermediate1 += state**2
+         intermediate2 += state**4
+         NNs = lattice.getNearestNeighbours(pos)
+         intermediate3 += (NNs[0]-NNs[2]+NNs[1]-NNs[3])
+
+    freeEn = (aConst/2) * intermediate1 + (aConst/4) * intermdiate2 + (kappa/(4*spatDist)) * intermediate3
+
+    return freeEn
 
 
 def main():
     dimensions, mobility, kappa, aConst, spatDisc, tempDisc, initVal = getInputParams()
 
     initgrid = generateInitState(dimensions, initVal)
-    lattice = PeriodicLattice(dimensions, 8, initgrid)
+    lattice = PeriodicLattice(dimensions, 4, initgrid)
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -67,6 +90,6 @@ def main():
 
         im.set_array(lattice.getLattice())
         fig.canvas.draw()
-        print(lattice.getLattice())
+        #print(lattice.getLattice())
 
 main()
